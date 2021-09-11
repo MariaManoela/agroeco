42bd75f2ce722d07d5b8a3b706f6a76b87cebbeb
Maria Manoela <manoelamvc@hotmail.com>
2021-09-11 17:48:44 -0300

    Add swiper

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev', 
+    pagination: {
+        el: '.swiper-pagination'
     },
-    mousewheel: true,
-    keyboard: true
+    // mousewheel: true,
+    // keyboard: true
 });
