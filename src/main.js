95b6205ba84315c615f65009ae078834e7e74687
Maria Manoela <manoelamvc@hotmail.com>
2021-09-12 11:15:24 -0300

    Delete javascript file

--- src/main.js
+++
@@ -1,8 +0,0 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    // mousewheel: true,
-    // keyboard: true
-});
